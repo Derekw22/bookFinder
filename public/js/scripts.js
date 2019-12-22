12d10e3592f9a8b342531cb4b7fc586e09e9a304
Derek <derekw22@gmail.com>
2019-12-22 13:08:02 +0200

    styling done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,7 +18,7 @@ function findBook(){
             image.src = book.items[i].volumeInfo.imageLinks.thumbnail;      
             var div = document.createElement("div");
             div.className = "media-body";
-            var header = document.createElement('h5');
+            var header = document.createElement('h2');
             header.className = 'mt-0';
             header.innerHTML = book.items[i].volumeInfo.title;
             div.appendChild(header);
@@ -29,13 +29,34 @@ function findBook(){
             author.innerHTML = "<b>Author:</b>   " + book.items[i].volumeInfo.authors;
             div.appendChild(author);
             //
-            var desc = document.createElement('p');
-            desc.innerHTML = book.items[i].volumeInfo.description;
-            div.appendChild(desc);
-            //
             var pageCount = document.createElement('p');
             pageCount.innerHTML = "<b>Page Count:</b>   " + book.items[i].volumeInfo.pageCount;
             div.appendChild(pageCount);
+            //
+            var published = document.createElement('p');
+            published.innerHTML = "<b>Published: </b>   " + book.items[i].volumeInfo.publishedDate;
+            div.appendChild(published);
+            //
+            var publisher = document.createElement('p');
+            publisher.innerHTML = "<b>Publisher:</b>   " + book.items[i].volumeInfo.publisher;
+            div.appendChild(publisher);
+             //
+             var rating = document.createElement('p');
+             rating.innerHTML = "<b>Average Rating: </b>   " + book.items[i].volumeInfo.averageRating;
+             div.appendChild(rating);
+             //
+             var rateCnt = document.createElement('p');
+             rateCnt.innerHTML = "<b>Rating Count: </b>   " + book.items[i].volumeInfo.ratingsCount;
+             div.appendChild(rateCnt);
+            //
+            var desc = document.createElement('p');
+            desc.innerHTML = book.items[i].volumeInfo.description;
+            div.appendChild(desc);
+            // PREVIEW LINK
+            var link = document.createElement('a');
+            link.innerHTML = "View More";
+            link.href = book.items[i].volumeInfo.previewLink;
+            div.appendChild(link);
             // line
             var line = document.createElement("hr"); 
             bookResult.appendChild(wrapperDiv);
